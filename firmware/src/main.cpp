ff64fd0c6c6644d24600b2e0d1dba650555e003e
maxnemoy <maxnemoy@gmail.com>
2022-01-01 14:28:40 +0800

    impl button

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -5,7 +5,9 @@
 #include "Wire.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
+#include "OneButton.h"
 
+OneButton button(25, true);
 Sensor sensor(4, 2);
 Rotary encoder(26, 27);
 Adafruit_SSD1306 display(128, 64, &Wire);
@@ -59,8 +61,45 @@ TaskHandle_t inputHandle;
   }
 }
 
+// This function will be called when the button1 was pressed 1 time (and no 2. button press followed).
+void click1() {
+  Serial.println("Button 1 click.");
+} // click1
+
+
+// This function will be called when the button1 was pressed 2 times in a short timeframe.
+void doubleclick1() {
+  Serial.println("Button 1 doubleclick.");
+} // doubleclick1
+
+
+// This function will be called once, when the button1 is pressed for a long time.
+void longPressStart1() {
+  Serial.println("Button 1 longPress start");
+} // longPressStart1
+
+
+// This function will be called often, while the button1 is pressed for a long time.
+void longPress1() {
+  Serial.println("Button 1 longPress...");
+} // longPress1
+
+
+// This function will be called once, when the button1 is released after beeing pressed for a long time.
+void longPressStop1() {
+  Serial.println("Button 1 longPress stop");
+} 
+
 void setup() {
   Serial.begin(115200);
+
+  button.attachClick(click1);
+  button.attachDoubleClick(doubleclick1);
+  button.attachLongPressStart(longPressStart1);
+  button.attachLongPressStop(longPressStop1);
+  button.attachDuringLongPress(longPress1);
+
+
   vTaskDelay(pdMS_TO_TICKS(100));
   if (xTaskCreatePinnedToCore(displayThread, "display", 2048,
                               nullptr, 1,
@@ -82,8 +121,10 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("mm:\t");
-  Serial.println(sensor.getMillimetersOfMercury() , 2);
-  Serial.print("psi:\t");
-  Serial.println(sensor.getPsi() , 2);
+  button.tick();
+  vTaskDelay(pdMS_TO_TICKS(10));
+  //Serial.print("mm:\t");
+  //Serial.println(sensor.getMillimetersOfMercury() , 2);
+  //Serial.print("psi:\t");
+  //Serial.println(sensor.getPsi() , 2);
 }
